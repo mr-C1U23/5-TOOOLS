ff4cbb6aefc82a0675742313393d5f8827c96a55
Mr-C1U23 <65049629+mr-C1U23@users.noreply.github.com>
2020-05-10 06:06:30 +0800

    Add files via upload

---
+++ 5tools.sh
@@ -0,0 +1,93 @@
+#!\bin\bash
+
+clear 
+toilet -f slant --gay "T00LS"
+date | lolcat
+echo
+echo "========================================"
+echo '\033[1;37m'
+echo "\033[1;35m Author : mr-C1U23"
+echo "\033[1;35m Github : https://github.com/mr-C1U23"
+echo '\033[1;37m'
+echo "========================================"
+echo
+echo "Menu Tools"
+echo
+echo "[1] HacK FB"
+echo "[2] Hack IG"
+echo "[3] Attack Hp"
+echo "[4] Spam SmS"
+echo "[5] Sadap WA v.6"
+echo "[6] Install bahan"
+echo				
+read -p "[?] Pilih : " pil;
+# Batas gan
+if [ $pil = 1 ]
+then
+sleep 5
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+cd hack-facebook-teman
+python2 hack-fb.py
+fi
+# Batas gan
+if [ $pil = 2 ]
+then
+sleep 5
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08
+sh Moreno77.sh
+fi
+# Batas gan
+if [ $pil = 3 ]
+then
+sleep 5
+git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+fi
+# Batas gan
+if [ $pil = 4 ]
+then
+sleep 5
+git clone https://github.com/Fukur0-3XP/SpamSms
+cd SpamSms
+python2 Spam.py
+fi
+# Batas gan
+if [ $pil = 5 ]
+then
+sleep 5
+git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsapp
+python2 HxWhatsApp.py
+fi
+# Batas gan
+if [ $pil = 6 ]
+then
+echo "\033[0;31m MEMULAI PENGINSTALAN....."
+sleep 5
+apt update && apt upgrade
+pip2 install --upgrade pipi
+pip2 install requests
+pip2 install mechanize
+pkg install git -y
+pkg install nano -y
+pkg install figlet -y
+pkg install python2 -y
+pip2 install lolcat -y
+pkg install cowsay -y
+pkg install update -y
+pkg install git python2 -y
+sleep 5
+echo "\033[0;31m PENGINSTALAN SELESAI....."
+fi
+
+
+
+
+
+
+
+
+
+
